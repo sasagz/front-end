72b26c5c4da53fbfba787ee13c9dc755773f12dd
sasagz_escola <163011955+sasagz@users.noreply.github.com>
2024-10-20 21:16:13 -0300

    .

--- script.js
+++ script.js
@@ -4,15 +4,18 @@ document.getElementById('cadastroForm').addEventListener('submit', function(even
     const nome = document.getElementById('nome').value;
     const email = document.getElementById('email').value;
 
-    const tabelaCadastro = document.getElementById('tabelaCadastro');
-    const novaLinha = tabelaCadastro.insertRow();
+    
+    const tabela = document.getElementById('tabela').getElementsByTagName('tbody')[0];
+    const novaLinha = tabela.insertRow();
 
+    
     const celulaNome = novaLinha.insertCell(0);
     const celulaEmail = novaLinha.insertCell(1);
-
+    
+    
     celulaNome.textContent = nome;
     celulaEmail.textContent = email;
 
-    // Limpar os campos do formulário
+    
     document.getElementById('cadastroForm').reset();
 });
